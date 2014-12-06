0c2b0f5a8e33257d481cea1bb09daf9e74f495ba
Al Wold <alwold@alwold.com>
2014-12-06 14:55:18 -0700

    Initial Commit

---
+++ Bus Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bus Finder
+//
+//  Created by Al Wold on 12/6/14.
+//  Copyright (c) 2014 Al Wold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
